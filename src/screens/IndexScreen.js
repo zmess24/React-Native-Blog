9b7fc6bc6fa2a9274a7249e37497cfe0fd7c6ea6
Zac Messsinger <zmessinger@quantummetric.com>
2019-11-10 18:03:11 -0800

    Added removeBlogPost

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,21 +1,47 @@
 import React, { useContext } from 'react';
-import { View, Text, Stylesheet, FlatList, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 export default IndexScreen = () => {
-    const { state, addBlogPost } = useContext(Context);
+    const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
     return (
         <View>
-            <Text>Index Screen</Text>
             <Button title="Add Post" onPress={addBlogPost}/>
             <FlatList
                 data={state}
                 keyExtractor={(blogPost) => blogPost.title}
                 renderItem={({ item }) => {
-                    return <Text>{item.title}</Text>                    
+                    return (
+                        <View style={styles.row}>
+                            <Text style={styles.title}>
+                                {item.title} - {item.id}
+                            </Text>
+                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                <Feather style={styles.icon} name="trash"/>
+                            </TouchableOpacity>
+                        </View>
+                    )
                 }}
             />
         </View>
     )
 };
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        borderTopWidth: 1,
+        paddingHorizontal: 10,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
+})
